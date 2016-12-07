ea4d7ab2470f310f9a17e1f136c17d6e85ab8292
Jdiesel87 <jdereniwski@gmail.com>
2016-12-07 14:08:18 -0600

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -17,9 +17,9 @@ define(['playbackManager', 'pluginManager', 'browser', 'connectionManager', 'eve
 
         var self = this;
 
-        self.name = 'Default Skin';
+        self.name = 'Jdiesel87';
         self.type = 'skin';
-        self.id = 'defaultskin';
+        self.id = 'jdiesel87';
 
         var dependencyPrefix = self.id;
         var settingsObjectName = dependencyPrefix + '/skinsettings';
