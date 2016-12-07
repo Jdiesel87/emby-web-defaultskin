de2b4321cea3540825b518a3cce13b49ae46c00f
Jdiesel87 <jdereniwski@gmail.com>
2016-12-07 16:20:13 -0600

    Update tv.js

--- tv/tv.js
+++ tv/tv.js
@@ -194,7 +194,7 @@ define(['connectionManager', 'loading', './../skininfo', 'alphaPicker', './../co
                     }
                 },
                 cardOptions: {
-                    shape: 'wide',
+                    shape: 'banner',
                     rows: 3,
                     preferBanner: true,
                     scalable: false
